754a3f4623434fa3d88a7c927943c951d082699e
Santiago Montoya <smonto67@gmail.com>
2021-10-14 09:17:44 -0500

    se adiciona el modulo usuarios

--- src/components/Usuarios.js
+++ src/components/Usuarios.js
@@ -1,13 +1,38 @@
 import React from "react";
 
-function Usuarios(){
-    return(
+function Usuarios() {
+    return (
         <>
-        <div class="content-wrapper">
-            <section class="content">
-            
-            </section>
-        </div>
+            <div class="content-wrapper">
+                <section class="content">
+                    <div class="container-fluid">
+                        <h1>Gestor de Usuarios y Roles</h1>
+                        <table id="TablaUsuarios" class="table table-striped">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th class="text-center">ID Usuario</th>
+                                    <th class="text-center">Nombre</th>
+                                    <th class="text-center">Rol</th>
+                                    <th class="text-center">Estado</th>
+                                    <th class="text-center">Accion</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th class="text-center">1</th>
+                                    <td class="text-center">Santiago Montoya P.</td>
+                                    <td class="text-center">Administrador</td>
+                                    <td class="text-center">Activo</td>
+                                    <td class="text-center">
+                                        <button id="btnEditar" ><i class="fas fa-eye"></i></button>
+                                        <button id="btnEliminar"><i class="fas fa-edit"></i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
