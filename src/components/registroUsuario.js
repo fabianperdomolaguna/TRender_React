ba6ff3f19b8ce9ba6d51d7d2495b7faf3c26ef55
Santiago Montoya <smonto67@gmail.com>
2021-10-21 09:37:46 -0500

    ya registra usuarios y lo lleva a base de datos

--- src/components/registroUsuario.js
+++ src/components/registroUsuario.js
@@ -1,40 +1,76 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { consultarDocumentoDatabase, actualizarDocumentoDatabase } from '../conexion-bd/funciones';
+import { guardarDatabase, actualizarDocumentoDatabase } from '../conexion-bd/funciones';
 import { Link, useHistory } from "react-router-dom";
 
-function registroUsuario (){
+function registroUsuario() {
+
+    const [nombre, setNombre] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [password, setPassword] = useState("");
+
+    const usuario = {
+        nombre,
+        correo,
+        password
+    }
     
+    const history=useHistory()
 
-    return(
+    const handleClick = async (e) => {
+        e.preventDefault();
+        if (nombre && correo && password) {
+            guardarDatabase('usuarios', usuario)
+            alert("Usuario registrado de forma exitosa")
+            history.push('/')
+        }else{
+            alert("Faltan campos por introducir")
+        }  
+    }
+
+    return (
         <>
-        <h3 className="text-center text-black pt-5">Registro</h3>
-        <div className="box-form mx-auto">
-            <form>
-                <div class="mb-3 container">
-                    <label for="TextInput" className="form-label">Ingrese su nombre completo:</label>
-                    <input type="text" id="Curso" className="form-control"/>
-                </div>
+            <h3 className="text-center text-black pt-5">Registro</h3>
+            <div className="box-form mx-auto">
+                <form>
+                    <div class="mb-3 container">
+                        <label for="TextInput" className="form-label">Nombre completo:</label>
+                        <input type="text" id="nombre" className="form-control"
+                            placeholder="Ingrese su nombre completo"
+                            value={nombre}
+                            onChange={(e) => setNombre(e.target.value)}
+                        />
+                    </div>
 
-                <div class="mb-3 container">
-                    <label for="TextInput" className="form-label">Ingrese su correo:</label>
-                    <input type="text" id="email" className="form-control"/>
-                </div>
+                    <div class="mb-3 container">
+                        <label for="TextInput" className="form-label">Correo:</label>
+                        <input type="text" id="email" className="form-control"
+                            placeholder="Ingrese su correo"
+                            value={correo}
+                            onChange={(e) => setCorreo(e.target.value)}
+                        />
+                    </div>
 
-                <div className="mb-3 container">
-                    <label for="TextInput" className="form-label">Ingrese su contraseña:</label>
-                    <input type="text" id="password" className="form-control"/>
-                </div>
+                    <div className="mb-3 container">
+                        <label for="TextInput" className="form-label">Contraseña:</label>
+                        <input type="password" id="password" className="form-control"
+                            placeholder="Ingrese su contraseña"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
 
-                <div className="container mx-auto">
-                        <button type="submit" className="btn btn-dark mr-3">Registrar</button>
-                    <Link to='/'>
-                        <button type="reset" className="btn btn-dark">Cancelar</button>
-                    </Link>
-                </div>
-            </form>
-        </div>
+                    <div className="container mx-auto">
+                        <button type="submit" className="btn btn-dark mr-3"
+                            onClick={handleClick}
+                        >Registrar</button>
+                        <Link to='/'>
+                            <button type="reset" className="btn btn-dark">Cancelar</button>
+                        </Link>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
