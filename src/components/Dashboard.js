413c92eb4ae792c744030bc621f1ad6937868db3
Santiago Montoya <smonto67@gmail.com>
2021-10-22 23:53:28 -0500

    ver los componentes solo con login

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,66 +1,38 @@
 import React from 'react';
+<<<<<<< HEAD
+import { Link, withRouter } from "react-router-dom"
+import { logOutUsuario } from '../conexion-bd/funciones';
+=======
 import { Link, NavLink, withRouter } from "react-router-dom"
+>>>>>>> 1ff370fa1b2ba5bb0f11b1d26207d4e8c2767851
+
+
+const handleLogOut = () => {
+    logOutUsuario()
+}
 
 
 
-// <button onClick={event =>  window.location.href='login.html'} type="button" class="btn btn-dark">
-//     <i class="fas fa-sign-in-alt"></i> Login
-// </button>
-
-// <button onClick={event =>  window.location.href='login.html'} type="button" 
-// class="btn btn-danger">Logout
-// </button>
-
-
-// const LoginButton = (props) => {
-//     const user_state = props.user;
-//     if (user_state){
-//         return (<button onClick={event =>  window.location.href='login.html'} type="button" class="btn btn-dark">
-//             <i class="fas fa-sign-in-alt"></i> Login
-//         </button>)
-//     } else{
-//         return (<button onClick={event =>  window.location.href='login.html'} type="button" class="btn btn-danger">Logout</button>)
-//     }
-// }
-
-/*const Menus_bar = (props) => {
-    let menu_name = props.menu_name;
-    const  menus ={
-        curso: ['/cursos', 'nav-icon fas fa-chart-pie', 'Cursos'],
-        venta: ['gestor_ventas.html', 'nav-icon fas fa-tree', 'Venta de Cursos'],
-        usuarios: ['gest_usuarios_roles.html', 'nav-icon fas fa-edit', 'Usuarios'],
-        roles: ['listar.html', 'nav-icon fas fa-table', 'Roles']
-    }
-    let values = menus[menu_name];
-    return (
-        <li class={`nav-item active dropdown ${props.location.pathname === "/" ? "active" : ""}`}>
-        <Link class="nav-link" to={values[0]}>
-                <i class={values[1]}></i>
-                <p>{values[2]}<i class="right fas fa-angle-right"></i></p>
-            </Link>
-        </li>
-    )
-
-}*/
-
-const Dashboard = ({usuario}) => {
+const Dashboard = ({ usuario }) => {
     console.log(usuario)
     return (
         <>
             <nav className="main-header navbar navbar-expand navbar-white navbar-light">
                 <ul className="navbar-nav ml-auto">
                     {
-                        usuario ? (
-                        <Link className="nav-item" to="/login">
-                            <button type="button" className="btn btn-dark">
-                                <i class="fas fa-sign-in-alt"></i> LogIn
-                            </button>
-                        </Link>)
-                        : 
-                        (<Link className="nav-item" to="/logout">
-                            <button type="button" className="btn btn-danger" >LogOut
-                            </button>
-                        </Link>)
+                        !usuario ? (
+                            <Link className="nav-item" to="/login">
+                                <button type="button" className="btn btn-dark">
+                                    <i class="fas fa-sign-in-alt"></i> LogIn
+                                </button>
+                            </Link>)
+                            :
+                            (<Link className="nav-item" to="/login">
+                                <button type="button" className="btn btn-danger"
+                                    onClick={handleLogOut}
+                                >LogOut
+                                </button>
+                            </Link>)
                     }
                 </ul>
             </nav>
@@ -70,17 +42,57 @@ const Dashboard = ({usuario}) => {
                     <img src="TRender.png" className="brand-image img-circle elevation-3" />
                     <span className="brand-text font-weight-light">Trender University</span>
                 </Link>
-
-                <div className="sidebar">
-                    <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                        <div className="image"><img src="User1.jpg" className="img-circle elevation-2" alt="User Image" /></div>
-                        <div className="info">
-                            <a href="#" className="d-block">USUARIO LOGUEADO</a>
+                {!usuario ? (
+                    <div className="sidebar">
+                        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+                            <div className="image"><img src="User1.jpg" className="img-circle elevation-2" alt="User Image" /></div>
+                            <div className="info">
+                                <a href="#" className="d-block">NO LOGUEADO</a>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
+                    </div>)
+                    :
+                    (
+                        <div className="sidebar">
+                            <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+                                <div className="image"><img src="User1.jpg" className="img-circle elevation-2" alt="User Image" /></div>
+                                <div className="info">
+                                    <a href="#" className="d-block">LOGUEADO</a>
+                                </div>
+                            </div>
+                        </div>)
+                }
                 <nav className="mt-2">
+<<<<<<< HEAD
+                    {!!usuario &&
+                        <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <li className='nav-item'>
+                                <Link className="nav-link" to="/cursos">
+                                    <i className='nav-icon fas fa-chart-pie'></i>
+                                    <p>Cursos<i className="right fas fa-angle-right"></i></p>
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className="nav-link" to="/">
+                                    <i className='nav-icon fas fa-tree'></i>
+                                    <p>Venta de Cursos<i className="right fas fa-angle-right"></i></p>
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className="nav-link" to="/usuarios">
+                                    <i className='nav-icon fas fa-edit'></i>
+                                    <p>Usuarios<i className="right fas fa-angle-right"></i></p>
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className="nav-link" to="/roles">
+                                    <i className='nav-icon fas fa-table'></i>
+                                    <p>Roles<i className="right fas fa-angle-right"></i></p>
+                                </Link>
+                            </li>
+                        </ul>
+                    }
+=======
                     <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         <li className='nav-item'>
                             <NavLink className="nav-link" to="/cursos">
@@ -107,6 +119,7 @@ const Dashboard = ({usuario}) => {
                             </NavLink>
                         </li>
                     </ul>
+>>>>>>> 1ff370fa1b2ba5bb0f11b1d26207d4e8c2767851
                 </nav>
             </aside>
         </>
