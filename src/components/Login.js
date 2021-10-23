a55363069257a93a535f4dace33378d85d8e02eb
Santiago Montoya <smonto67@gmail.com>
2021-10-23 01:11:04 -0500

    diseño de botones de login

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,9 +5,9 @@ import { loginUsuario } from "../conexion-bd/funciones";
 
 function Login() {
 
-    const[email,setEmail] = useState("");
-    const[password,setPassword] = useState("");
-    const history= useHistory()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const history = useHistory()
 
     const handleClick = async (e) => {
         e.preventDefault();
@@ -18,9 +18,9 @@ function Login() {
             } catch (error) {
                 alert("Usuario no existe. Revisar sus credenciales")
             }
-        }else{
+        } else {
             alert("Usuario o contraseña invalidos")
-        }  
+        }
     }
 
 
@@ -38,29 +38,31 @@ function Login() {
                                     <div class="form-group first">
                                         <label class="text-black" for="username">Correo</label>
                                         <input type="text" className="form-control" placeholder="Ingresa tu correo" id="username"
-                                        required 
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}/>
+                                            required
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)} />
                                     </div>
 
                                     <div class="form-group last mb-3">
                                         <label class="text-black" for="password">Contraseña</label>
                                         <input type="password" className="form-control" placeholder="Ingresa tu contraseña" id="password"
-                                        required
-                                        value={password} 
-                                        onChange={(e) => setPassword(e.target.value)}/>
+                                            required
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)} />
                                     </div>
 
+                                    <input type="submit" value="ACCEDER" class="btn btn-block btn-outline-success"
+                                        onClick={handleClick}
+                                    />
+                                    <br />
                                     <div class="social-author">
                                         <Link class="btn btn-sm btn-outline-info text-black"
                                             to={`/registro/:nuevousuario`}>
                                             Registrarse
                                         </Link>
                                     </div>
-                                    <br />
-                                    <input type="submit" value="ACCEDER" class="btn btn-block btn-secondary"
-                                    onClick={handleClick}
-                                    />
+
+
                                 </form>
                             </div>
                         </div>
